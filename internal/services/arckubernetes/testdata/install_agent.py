12d38bbf2b8a82b485c87ffa05c37184041cb419
Sourcery AI <>
2023-09-02 14:56:28 +0000

    'Refactored by Sourcery'

--- internal/services/arckubernetes/testdata/install_agent.py
+++ internal/services/arckubernetes/testdata/install_agent.py
@@ -21,20 +21,20 @@ Pre_Onboarding_Helm_Charts_Folder_Name = 'PreOnboardingChecksCharts'
 
 def get_helm_registry(config_dp_endpoint):
     # Setting uri
-    get_chart_location_url = "{}/{}/GetLatestHelmPackagePath?api-version=2019-11-01-preview".format(
-        config_dp_endpoint, 'azure-arc-k8sagents')
+    get_chart_location_url = f"{config_dp_endpoint}/azure-arc-k8sagents/GetLatestHelmPackagePath?api-version=2019-11-01-preview"
 
     try:
         response = urllib.request.urlopen(
             request.Request(get_chart_location_url, method="POST"))
     except Exception as e:
-        raise Exception("Failed to get helm registry." + str(e))
+        raise Exception(f"Failed to get helm registry.{str(e)}")
 
     try:
         return json.load(response).get('repositoryPath')
     except Exception as e:
         raise Exception(
-            "Error while fetching helm chart registry path from JSON response: " + str(e))
+            f"Error while fetching helm chart registry path from JSON response: {str(e)}"
+        )
 
 
 def pull_helm_chart(registry_path, kube_config, kube_context, helm_client_location, chart_name='azure-arc-k8sagents', retry_count=5, retry_delay=3):
@@ -50,8 +50,12 @@ def pull_helm_chart(registry_path, kube_config, kube_context, helm_client_locati
         _, error_helm_chart_pull = response_helm_chart_pull.communicate()
         if response_helm_chart_pull.returncode != 0:
             if i == retry_count - 1:
-                raise Exception("Unable to pull {} helm chart from the registry '{}': ".format(
-                    chart_name, registry_path) + error_helm_chart_pull.decode("ascii"))
+                raise Exception(
+                    (
+                        f"Unable to pull {chart_name} helm chart from the registry '{registry_path}': "
+                        + error_helm_chart_pull.decode("ascii")
+                    )
+                )
             time.sleep(retry_delay)
         else:
             break
@@ -68,8 +72,12 @@ def export_helm_chart(registry_path, chart_export_path, kube_config, kube_contex
         cmd_helm_chart_export, stdout=PIPE, stderr=PIPE)
     _, error_helm_chart_export = response_helm_chart_export.communicate()
     if response_helm_chart_export.returncode != 0:
-        raise Exception("Unable to export {} helm chart from the registry '{}': ".format(
-            chart_name, registry_path) + error_helm_chart_export.decode("ascii"))
+        raise Exception(
+            (
+                f"Unable to export {chart_name} helm chart from the registry '{registry_path}': "
+                + error_helm_chart_export.decode("ascii")
+            )
+        )
 
 
 def get_chart_path(registry_path, kube_config, kube_context, helm_client_location, chart_folder_name='AzureArcCharts', chart_name='azure-arc-k8sagents'):
@@ -85,8 +93,9 @@ def get_chart_path(registry_path, kube_config, kube_context, helm_client_locatio
         if os.path.isdir(chart_export_path):
             shutil.rmtree(chart_export_path)
     except:
-        logger.warning("Unable to cleanup the {} already present on the machine. In case of failure, please cleanup the directory '{}' and try again.".format(
-            chart_folder_name, chart_export_path))
+        logger.warning(
+            f"Unable to cleanup the {chart_folder_name} already present on the machine. In case of failure, please cleanup the directory '{chart_export_path}' and try again."
+        )
 
     export_helm_chart(registry_path, chart_export_path, kube_config,
                       kube_context, helm_client_location, chart_name)
@@ -94,12 +103,13 @@ def get_chart_path(registry_path, kube_config, kube_context, helm_client_locatio
     # Returning helm chart path
     helm_chart_path = os.path.join(chart_export_path, chart_name)
     if chart_folder_name == Pre_Onboarding_Helm_Charts_Folder_Name:
-        chart_path = helm_chart_path
+        return helm_chart_path
     else:
-        chart_path = os.getenv('HELMCHART') if os.getenv(
-            'HELMCHART') else helm_chart_path
-
-    return chart_path
+        return (
+            os.getenv('HELMCHART')
+            if os.getenv('HELMCHART')
+            else helm_chart_path
+        )
 
 
 def install_helm_client():
@@ -112,7 +122,7 @@ def install_helm_client():
         download_location_string = f'.azure\\helm\\{HELM_VERSION}\\helm-{HELM_VERSION}-{operating_system}-amd64.zip'
         install_location_string = f'.azure\\helm\\{HELM_VERSION}\\{operating_system}-amd64\\helm.exe'
         requestUri = f'{HELM_STORAGE_URL}/helm/helm-{HELM_VERSION}-{operating_system}-amd64.zip'
-    elif (operating_system == 'linux' or operating_system == 'darwin'):
+    elif operating_system in ['linux', 'darwin']:
         download_location_string = f'.azure/helm/{HELM_VERSION}/helm-{HELM_VERSION}-{operating_system}-amd64.tar.gz'
         install_location_string = f'.azure/helm/{HELM_VERSION}/{operating_system}-amd64/helm'
         requestUri = f'{HELM_STORAGE_URL}/helm/helm-{HELM_VERSION}-{operating_system}-amd64.tar.gz'
@@ -133,7 +143,7 @@ def install_helm_client():
             try:
                 os.makedirs(download_dir)
             except Exception as e:
-                raise Exception("Failed to create helm directory." + str(e))
+                raise Exception(f"Failed to create helm directory.{str(e)}")
 
         # Downloading compressed helm client executable
         logger.warning(
@@ -141,7 +151,7 @@ def install_helm_client():
         try:
             response = urllib.request.urlopen(requestUri)
         except Exception as e:
-            raise Exception("Failed to download helm client." + str(e))
+            raise Exception(f"Failed to download helm client.{str(e)}")
 
         responseContent = response.read()
         response.close()
@@ -151,7 +161,7 @@ def install_helm_client():
             with open(download_location, 'wb') as f:
                 f.write(responseContent)
         except Exception as e:
-            raise Exception("Failed to create helm executable." + str(e))
+            raise Exception(f"Failed to create helm executable.{str(e)}")
 
     # Extract compressed helm binary
     if not os.path.isfile(install_location):
@@ -160,7 +170,7 @@ def install_helm_client():
             os.chmod(install_location, os.stat(
                 install_location).st_mode | stat.S_IXUSR)
         except Exception as e:
-            raise Exception("Failed to extract helm executable." + str(e))
+            raise Exception(f"Failed to extract helm executable.{str(e)}")
 
     return install_location
 
@@ -168,34 +178,45 @@ def install_helm_client():
 def helm_install_release(chart_path, subscription_id, kubernetes_distro, kubernetes_infra, resource_group_name, cluster_name,
                          location, onboarding_tenant_id, private_key_pem,
                          no_wait, cloud_name, helm_client_location, onboarding_timeout="600"):
-    cmd_helm_install = [helm_client_location, "upgrade", "--install", "azure-arc", chart_path,
-                        "--set", "global.subscriptionId={}".format(
-                            subscription_id),
-                        "--set", "global.kubernetesDistro={}".format(
-                            kubernetes_distro),
-                        "--set", "global.kubernetesInfra={}".format(
-                            kubernetes_infra),
-                        "--set", "global.resourceGroupName={}".format(
-                            resource_group_name),
-                        "--set", "global.resourceName={}".format(cluster_name),
-                        "--set", "global.location={}".format(location),
-                        "--set", "global.tenantId={}".format(
-                            onboarding_tenant_id),
-                        "--set", "global.onboardingPrivateKey={}".format(
-                            private_key_pem),
-                        "--set", "systemDefaultValues.spnOnboarding=false",
-                        "--set", "global.azureEnvironment={}".format(
-                            cloud_name),
-                        "--set", "systemDefaultValues.clusterconnect-agent.enabled=true",
-                        "--namespace", "{}".format("azure-arc-release"),
-                        "--create-namespace",
-                        "--output", "json"]
+    cmd_helm_install = [
+        helm_client_location,
+        "upgrade",
+        "--install",
+        "azure-arc",
+        chart_path,
+        "--set",
+        f"global.subscriptionId={subscription_id}",
+        "--set",
+        f"global.kubernetesDistro={kubernetes_distro}",
+        "--set",
+        f"global.kubernetesInfra={kubernetes_infra}",
+        "--set",
+        f"global.resourceGroupName={resource_group_name}",
+        "--set",
+        f"global.resourceName={cluster_name}",
+        "--set",
+        f"global.location={location}",
+        "--set",
+        f"global.tenantId={onboarding_tenant_id}",
+        "--set",
+        f"global.onboardingPrivateKey={private_key_pem}",
+        "--set",
+        "systemDefaultValues.spnOnboarding=false",
+        "--set",
+        f"global.azureEnvironment={cloud_name}",
+        "--set",
+        "systemDefaultValues.clusterconnect-agent.enabled=true",
+        "--namespace",
+        'azure-arc-release',
+        "--create-namespace",
+        "--output",
+        "json",
+    ]
 
     if not no_wait:
         # Change --timeout format for helm client to understand
-        onboarding_timeout = onboarding_timeout + "s"
-        cmd_helm_install.extend(
-            ["--wait", "--timeout", "{}".format(onboarding_timeout)])
+        onboarding_timeout = f"{onboarding_timeout}s"
+        cmd_helm_install.extend(["--wait", "--timeout", f"{onboarding_timeout}"])
     response_helm_install = Popen(cmd_helm_install, stdout=PIPE, stderr=PIPE)
     _, error_helm_install = response_helm_install.communicate()
     if response_helm_install.returncode != 0:
@@ -215,20 +236,20 @@ def install_agent():
     try:
         args = parser.parse_args()
     except Exception as e:
-        raise Exception("Failed to parse arguments." + str(e))
+        raise Exception(f"Failed to parse arguments.{str(e)}")
 
     try:
         with open(args.privatePem, "r") as f:
             privateKey = f.read()
     except Exception as e:
-        raise Exception("Failed to get private key." + str(e))
+        raise Exception(f"Failed to get private key.{str(e)}")
 
     # Install helm client
     helm_client_location = install_helm_client()
 
     # Retrieving Helm chart OCI Artifact location
     registry_path = get_helm_registry("https://westeurope.dp.kubernetesconfiguration.azure.com")
-    
+
     # Get helm chart path
     chart_path = get_chart_path(
         registry_path, None, None, helm_client_location)
